e5c0fb43b59ce9822010e3cf3ec60632ec31811d
Felipe Marinho <felipevm97@gmail.com>
2017-10-30 00:13:52 -0300

    us1 first part

--- si-lab1/src/js/app/app.module.js
+++ si-lab1/src/js/app/app.module.js
@@ -6,5 +6,6 @@ angular.module('musicoteca', [
   'ngResource',
 
   // interno
+  'artistInput'
 
 ]);
---
+++ si-lab1/src/js/app/artist-input/artist-input.component.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('artistInput').
+  component('artistInput', {
+    templateUrl: '/templates/artist-input.html',
+    controller: function($scope) {
+      
+    }
+  });
